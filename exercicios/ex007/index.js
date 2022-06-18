394380d6d6540d0285bd18e577fcd16f6feeabbd
KallelCHZS <102880413+KallelCHZS@users.noreply.github.com>
2022-06-18 07:59:18 -0300

    ..

---
+++ exercicios/ex007/index.js
@@ -0,0 +1,7 @@
+var vel= 60.5
+console.log(`O seu carro estas a ${vel}km/h`)
+if(vel > 60 )//CONDIÇÃO SIMPLES!
+{
+    console.log('Você excedel o limite de velocidade da via, você foi MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
