ea54618f8aa4f01a1662c54973977021bd1f2fa8
KallelCHZS <102880413+KallelCHZS@users.noreply.github.com>
2022-06-18 17:04:47 -0300

    ex

--- exercicios/ex007/index01.js
+++ exercicios/ex007/index01.js
@@ -2,6 +2,7 @@ var pais= 'Brasil'
 console.log(`Vivendo em ${pais}!`)
 if (pais == 'Brasil'){
     console.log('Você esta morando no Brasil!')
-}else{
+}
+else{
     console.log('Você esta morando no exterior!')
 }
