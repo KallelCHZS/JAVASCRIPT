11ff93a2ddb6bdcf711cafcac3af34f676536f4f
KallelCHZS <102880413+KallelCHZS@users.noreply.github.com>
2022-06-26 15:28:54 -0300

    ex16

---
+++ exercicios/ex016/index01.js
@@ -0,0 +1,5 @@
+var num = [5,8,4,9,3]
+num[5]=6
+num.push (7)
+
+console.log(`nosso vetor é ${num}`)
---
+++ exercicios/ex016/index03.js
@@ -0,0 +1,5 @@
+var num = [5,3,3,2,7,8,9]
+console.log(num)
+for (var pos = 0; pos<num.length; pos ++){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
